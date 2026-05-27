a09fdd6766592a5811cef8c787257fc5db36914a
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-05-27 09:47:50 +0400

    chore: main css setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from '@/store'
 import App from './App'
-import '@/assets/styles/global.css'
+import '../main.css'
 
 async function enableMocking() {
   if (!import.meta.env.DEV) return
